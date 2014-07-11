1f5709ac4617ce2e10a68bee0061d519ed05f785
SRINI <jsrinivasanmca@gmail.com>
2014-07-11 16:19:00 +0530

    Init AgeCalc

---
+++ AgeCalculator/src/com/example/agecalculator/AgeCalculation.java
@@ -0,0 +1,84 @@
+package com.example.agecalculator;
+
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class AgeCalculation {
+private int startYear;
+private int startMonth;
+
+private Calendar end;
+public String getCurrentDate()
+{
+end=Calendar.getInstance();
+endYear=end.get(Calendar.YEAR);
+endMonth=end.get(Calendar.MONTH);
+endMonth++;
+endDay=end.get(Calendar.DAY_OF_MONTH);
+return endDay+":"+endMonth+":"+endYear;
+}
+public void setDateOfBirth(int sYear, int sMonth, int sDay)
+
+
+}
+public void calcualteYear()
+{
+resYear=endYear-startYear;
+
+}
+
+public void calcualteMonth()
+{
+if(endMonth>=startMonth)
+{
+resMonth= endMonth-startMonth;
+}
+else
+{
+resMonth=endMonth-startMonth;
+resMonth=12+resMonth;
+resYear--;
+}
+
+}
+public void calcualteDay()
+{
+
+if(endDay>=startDay)
+{
+resDay= endDay-startDay;
+}
+else
+{
+resDay=endDay-startDay;
+resDay=30+resDay;
+if(resMonth==0)
+{
+resMonth=11;
+resYear--;
+}
+
+
+}
+}
+
+public String getResult()
+{
+return resDay+":"+resMonth+":"+resYear;
+}
+public long getSeconde()
+{
+start=Calendar.getInstance();
+start.set(Calendar.YEAR, startYear);
+start.set(Calendar.MONTH, startMonth);
+start.set(Calendar.DAY_OF_MONTH, startDay);
+start.set(Calendar.HOUR, 12);
+start.set(Calendar.MINUTE, 30);
+start.set(Calendar.MILLISECOND, 30);
+long now=end.getTimeInMillis();
+long old=start.getTimeInMillis();
+long diff=old-now;
+return diff/1000;
+}
+}
